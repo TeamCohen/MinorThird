0624ed2288c75c3982cc0d5de8fd53ac7ca4a82c
Kevin Steppe <ksteppe@cs.cmu.edu>
2004-01-14 19:13:42 +0000

    removing setup-tagger files

--- script/setup-tagger.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-echo WINDOWS setup script for MontyTagger
-echo Run this after the main KLINGER setup script has been run
-echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-set MONTYLINGUA=%KLINGER%/plugins/montytagger
--- script/setup-tagger.sh
+++
@@ -1,7 +0,0 @@
-echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-echo CYGWIN setup script for MontyTagger
-echo Run this after the main KLINGER setup script has been run
-echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-echo
-export MONTYLINGUA="${KLINGER-.}/plugins/montytagger"
-export CLASSPATH="${CLASSPATH};${MONTYLINGUA}/montylingua.jar"
