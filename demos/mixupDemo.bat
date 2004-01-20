9f9502d54816d7cce928ca3e74c8cea1b40a2dd9
Kevin Steppe <ksteppe@cs.cmu.edu>
2004-01-20 22:12:13 +0000

    sample mixup in 3 stages with comments

---
+++ demos/mixupDemo.bat
@@ -0,0 +1,13 @@
+@echo off
+echo + This demo displays the results of a few versions of a mixup file.
+echo + It uses the class MixupDebugger to evaluate and display results.
+echo + 
+echo + This demo uses sampleData/airtimeExample.txt for data.
+echo +
+@echo on
+
+java edu.cmu.minorthird.text.gui.MixupDebugger -file sampleData\airtimeExample.txt -mixup sampleMixup\timev-1.mixup
+
+java edu.cmu.minorthird.text.gui.MixupDebugger -file sampleData\airtimeExample.txt -mixup sampleMixup\timev-2.mixup
+
+java edu.cmu.minorthird.text.gui.MixupDebugger -file sampleData\airtimeExample.txt -mixup sampleMixup\time.mixup
