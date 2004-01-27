e8f3ae119204f6277a2751b5708c352ebf309be6
Kevin Steppe <ksteppe@cs.cmu.edu>
2004-01-27 19:53:40 +0000

    removed double spacing

--- src/edu/cmu/minorthird/classify/experiments/StratifiedCrossValSpiltter.java
+++ src/edu/cmu/minorthird/classify/experiments/StratifiedCrossValSpiltter.java
@@ -1,1 +1,72 @@
-package edu.cmu.minorthird.classify.experiments;import edu.cmu.minorthird.classify.Splitter;import edu.cmu.minorthird.classify.algorithms.random.RandomElement;import org.apache.log4j.Logger;import java.util.ArrayList;import java.util.Iterator;import java.util.List;/** * Works with datasets of binary examples.  Splits POS and NEG examples into * k separate disjoints folds, separately, and then returns k train/test splits * where each train set is the union of k-1 folds, and the test set is the k-th * fold.  Does NOT preserve subpopulation information. * * @author Edoardo Airoldi * Date: Dec 8, 2003 */public class StratifiedCrossValSpiltter implements Splitter {    static private Logger log = Logger.getLogger(StratifiedCrossValSpiltter.class);    private RandomElement random;    private int folds;    private List strata;    public StratifiedCrossValSpiltter(RandomElement random, int folds) {        this.random = random; this.folds = folds;    }    public StratifiedCrossValSpiltter(int folds) {        this(new RandomElement(), folds);    }    public StratifiedCrossValSpiltter() {        this(new RandomElement(), 5);    }    public void split(Iterator i) {        strata = new ArrayList();        for (Iterator j = new StrataSorter(random,i).strataIterator(); j.hasNext(); ) {            strata.add( j.next() );        }    }    public int getNumPartitions() { return folds; }    public Iterator getTrain(int k) {        List trainList = new ArrayList();        for (int i=0; i<strata.size(); i++) {            for (int j=0; j<((List)strata.get(i)).size(); j++) {                if (j%folds != k) {                    trainList.add( ((List)strata.get(i)).get(j) );                }            }        }        return trainList.iterator();    }    public Iterator getTest(int k) {        List testList = new ArrayList();        for (int i=0; i<strata.size(); i++) {            for (int j=0; j<((List)strata.get(i)).size(); j++) {                if (j%folds == k) {                    testList.add( ((List)strata.get(i)).get(j) );                }            }        }        return testList.iterator();    }    public String toString() { return "["+folds+"-Stratified CV splitter]"; }}
+package edu.cmu.minorthird.classify.experiments;
+
+import edu.cmu.minorthird.classify.Splitter;
+import edu.cmu.minorthird.classify.algorithms.random.RandomElement;
+import org.apache.log4j.Logger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Works with datasets of binary examples.  Splits POS and NEG examples into
+ * k separate disjoints folds, separately, and then returns k train/test splits
+ * where each train set is the union of k-1 folds, and the test set is the k-th
+ * fold.  Does NOT preserve subpopulation information.
+ *
+ * @author Edoardo Airoldi
+ * Date: Dec 8, 2003
+ */
+
+public class StratifiedCrossValSpiltter implements Splitter {
+
+    static private Logger log = Logger.getLogger(StratifiedCrossValSpiltter.class);
+
+    private RandomElement random;
+    private int folds;
+    private List strata;
+
+    public StratifiedCrossValSpiltter(RandomElement random, int folds) {
+        this.random = random; this.folds = folds;
+    }
+    public StratifiedCrossValSpiltter(int folds) {
+        this(new RandomElement(), folds);
+    }
+    public StratifiedCrossValSpiltter() {
+        this(new RandomElement(), 5);
+    }
+
+    public void split(Iterator i) {
+        strata = new ArrayList();
+        for (Iterator j = new StrataSorter(random,i).strataIterator(); j.hasNext(); ) {
+            strata.add( j.next() );
+        }
+    }
+
+    public int getNumPartitions() { return folds; }
+
+    public Iterator getTrain(int k) {
+        List trainList = new ArrayList();
+        for (int i=0; i<strata.size(); i++) {
+            for (int j=0; j<((List)strata.get(i)).size(); j++) {
+                if (j%folds != k) {
+                    trainList.add( ((List)strata.get(i)).get(j) );
+                }
+            }
+        }
+        return trainList.iterator();
+    }
+
+    public Iterator getTest(int k) {
+        List testList = new ArrayList();
+        for (int i=0; i<strata.size(); i++) {
+            for (int j=0; j<((List)strata.get(i)).size(); j++) {
+                if (j%folds == k) {
+                    testList.add( ((List)strata.get(i)).get(j) );
+                }
+            }
+        }
+        return testList.iterator();
+    }
+    public String toString() { return "["+folds+"-Stratified CV splitter]"; }
+
+}
