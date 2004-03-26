3a657bf11f9a720c35628b8e2913395d58053fc7
William W. Cohen <wcohen@cs.cmu.edu>
2004-03-26 21:56:51 +0000

    update

--- demos/mixupDemo.bat
+++ demos/mixupDemo.bat
@@ -1,4 +1,6 @@
 @echo off
+echo + This demo seems to be broken now, try another one.
+goto end
 echo + This demo displays the results of a few versions of a mixup file.
 echo + It uses the class MixupDebugger to evaluate and display results.
 echo + 
@@ -11,3 +13,5 @@ java edu.cmu.minorthird.text.gui.MixupDebugger -file sampleData\airtimeExample.t
 java edu.cmu.minorthird.text.gui.MixupDebugger -file sampleData\airtimeExample.txt -mixup sampleMixup\timev-2.mixup
 
 java edu.cmu.minorthird.text.gui.MixupDebugger -file sampleData\airtimeExample.txt -mixup sampleMixup\time.mixup
+
+:end
