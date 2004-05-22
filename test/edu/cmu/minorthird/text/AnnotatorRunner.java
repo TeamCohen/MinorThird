94fc40455fd250853ff9bda3686637d8dba85ef2
Vitor Rocha de Carvalho <vitor@cs.cmu.edu>
2004-05-22 14:15:28 +0000

    adding comments about new email annotators in email package (sig, replyto, etc)

--- test/edu/cmu/minorthird/text/AnnotatorRunner.java
+++ test/edu/cmu/minorthird/text/AnnotatorRunner.java
@@ -1,18 +1,18 @@
 package edu.cmu.minorthird.text;
 
 import edu.cmu.minorthird.text.*;
-import email.SigFileAnnotator;
-import email.ReplyToAnnotator;
-import org.apache.log4j.Logger;
- 
+import edu.cmu.minorthird.text.gui.*;
+//uncomment, add to classpath, and compile apps/email package
+//if you want to use these annotators 
+//import email.SigFileAnnotator;
+//import email.ReplyToAnnotator;
+import org.apache.log4j.Logger; 
 import java.io.File;
 
-/**
- * This class tests the usage of the String annotators
- * (SigFile, ReplyTo, POS, etc)
- * 
+/** 
+ * This class...
  * @author ksteppe
- */
+ */ 
 public class AnnotatorRunner
 {
   private static Logger log = Logger.getLogger(AnnotatorRunner.class);
@@ -28,17 +28,18 @@ public class AnnotatorRunner
       labels = TextBaseLoader.loadDirOfTaggedFiles(dir);
 
      //for sig annotations:
-      Annotator annotator = new SigFileAnnotator();
-      String tag = "sig";
+      //Annotator annotator = new SigFileAnnotator();
+      //String tag = "sig";
       
       //in case you only want the reply-to lines
       //Annotator annotator = new ReplyToAnnotator();
       //String tag = "reply";
       
       //for POS experiments
-      // Annotator annotator = new POSTagger();
+       Annotator annotator = new POSTagger();
+       String tag = "whatever";       
       
-      annotator.annotate(labels);
+       annotator.annotate(labels);
 
       // to see the results
       for (Span.Looper i = labels.instanceIterator(tag); i.hasNext();)
@@ -46,7 +47,7 @@ public class AnnotatorRunner
         Span span = i.nextSpan();
         System.out.println(span.toString().replace('\n', ' '));
       }
-
+      
     }
     catch (Exception e)
     {
