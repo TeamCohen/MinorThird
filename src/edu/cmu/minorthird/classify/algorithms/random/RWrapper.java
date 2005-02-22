f666ed6089c69f00bf572073ede3091ce334b35c
Edoardo Airoldi <edo@cs.cmu.edu>
2005-02-22 16:52:09 +0000

    Feb 22nd, 2005
     Corrected the error in SampleDatasetClassificaitonTest,java
    
    Edo

--- src/edu/cmu/minorthird/classify/algorithms/random/RWrapper.java
+++ src/edu/cmu/minorthird/classify/algorithms/random/RWrapper.java
@@ -7,21 +7,34 @@ package edu.cmu.minorthird.classify.algorithms.random;
 
 public interface RWrapper {
 
+    /** Teminates the REngine */
     public void end();
 
     //
     // Random Number Generators
     //
 
+    /** Genrates a vector of n doubles distributed according to a
+     *  Gaussian with: mean = mu and standard deviation = sd */
     public double [] rnorm(int n, double mu, double sd);
+    /** Genrates a vector of n doubles distributed according to a
+     *  Uniform with range: min = min and max = max */
     public double [] runif(int n, double min, double max);
+    /** Genrates a vector of n doubles distributed according to a
+     *  Binomial with parameters: mu = mu and delta = delta */
     public double [] rbinom(int n, double mu, double delta);
+    /** Genrates a vector of n doubles distributed according to a
+     *  Negative-Binomial with parameters: mu = mu and delta = delta */
     public double [] rnbinom(int n, double mu, double delta);
 
     //
     // Densities
     //
 
+    /** Evasluates a Gaussian density (mean = mu, standard deviation = sd)
+     *  at each element of the vector t */
     public double [] dnorm(double[] t, double mu, double sd, String string);
+    /** Evasluates a Gaussian density (mean = mu, standard deviation = sd)
+     *  at t */
     public double dnorm(double t, double mu, double sd, String string);
 }
