bf3b95b21733ed84514db31789f6af94b9ea7fc5
Vitor Rocha de Carvalho <vitor@cs.cmu.edu>
2005-03-07 17:34:37 +0000

    chi square statistic

---
+++ src/edu/cmu/minorthird/classify/transform/ChiSquareInstanceTransform.java
@@ -0,0 +1,121 @@
+package edu.cmu.minorthird.classify.transform;
+
+import edu.cmu.minorthird.classify.*;
+import java.util.*;
+
+/**
+ * @author Vitor R. Carvalho
+ * Date: March 2005
+ * 
+ * A simple feature filter based on Chi-squared statistic.
+ */
+
+public class ChiSquareInstanceTransform extends AbstractInstanceTransform
+{
+
+  private int TOP_FEATURES;
+  private ArrayList featValues;//keeps all features
+  private Set availableFeatures = new HashSet(); //keeps high score features
+  private boolean isSorted;
+
+
+  /** Default Constructor - numFeatures=100 */
+  public ChiSquareInstanceTransform ()
+  {
+    this.TOP_FEATURES = 100;  // default
+    this.featValues = new ArrayList();
+  }
+  /** Constructor - parameter is number of features */
+  public ChiSquareInstanceTransform (int num)
+  {
+    this.TOP_FEATURES = num;  // number of top ranked features
+    this.featValues = new ArrayList();
+  }
+
+  /** Transform an instance according to Info-Gain criterion */
+  public Instance transform(Instance ins)
+  {
+    // collect features to keep
+    if(!isSorted){
+        Collections.sort( featValues, VAL_COMPARATOR);
+        isSorted = true;
+        int maxIndex = Math.min( featValues.size(),TOP_FEATURES );
+//        availableFeatures = new HashSet();
+        for (int j=0; j<maxIndex; j++)
+        {
+            availableFeatures.add( ((Pair)featValues.get(j)).feature );
+        }
+    }
+    return new MaskedInstance(ins, availableFeatures);
+  }
+
+
+  /** A class that we use to sort a TreeMap by values */
+  private class Pair extends Object {
+    double value;
+    Feature feature;
+
+    public Pair(double v, Feature f) {
+      this.value = v;
+      this.feature = f;
+    }
+
+    public String toString() {
+      return "[ " + this.value + "," + this.feature + " ]"; //this.key + " ]";
+    }
+  }
+  
+  final Comparator VAL_COMPARATOR = new Comparator() {
+      public int compare(Object o1, Object o2) {
+        Pair ig1 = (Pair)o1;
+        Pair ig2 = (Pair)o2;
+        if (ig1.value<ig2.value) return 1;
+        else if (ig1.value>ig2.value) return -1;
+        else return (ig1.feature).compareTo( ig2.feature );
+      }
+    };
+    
+    
+  /** displays the top ranked features and their scores*/
+  public String toString(int top){
+      Collections.sort( featValues, VAL_COMPARATOR);
+      int maxIndex = Math.min( featValues.size(),top );
+      StringBuffer sb = new StringBuffer();
+      for (int j=0; j<maxIndex; j++)
+      {
+          Feature f = ((Pair)featValues.get(j)).feature;
+          double val = ((Pair)featValues.get(j)).value;
+          sb.append(f.toString()+" , "+val+"\n");
+      }
+      return sb.toString();
+  }
+  
+  /** get top ranked features*/
+  public Feature[] getTopFeatures(int top){
+      Collections.sort( featValues, VAL_COMPARATOR);
+      int maxIndex = Math.min( featValues.size(),top );
+      Feature[] ff = new Feature[maxIndex];
+      for (int j=0; j<maxIndex; j++)
+      {
+          ff[j] = ((Pair)featValues.get(j)).feature;
+      }
+      return ff;
+  }
+
+
+  /** Number of features with the highest scores to keep in the dataset */
+  public void setNumberOfFeatures(int number)
+  {
+    this.TOP_FEATURES = number;
+    isSorted=false;
+  }
+
+  /** Adds a score of feature f to the InstanceTransform */
+  public void addFeature(double infoGain, Feature f)
+  {
+    Pair p = new Pair( infoGain,f );
+    featValues.add( p );
+    isSorted=false;
+  }
+
+}
---
+++ src/edu/cmu/minorthird/classify/transform/ChiSquareTransformLearner.java
@@ -0,0 +1,93 @@
+package edu.cmu.minorthird.classify.transform;
+
+import edu.cmu.minorthird.classify.*;
+
+/**
+ * @author Vitor R. Carvalho
+ * Date: March 2005
+ *
+ *  A simple feature filter based on the Chi-Squared statistic.
+ *  The frequency model is resposible for deciding 'what to count'.  If set to
+ *  "document" this filter counts the number of documents which contain a Feature;
+ *  if set to "word" this filter counts the number of times a Feature appears in
+ *  the whole dataset.
+ */
+public class ChiSquareTransformLearner implements InstanceTransformLearner
+{
+  private String frequencyModel;
+
+  public ChiSquareTransformLearner()
+  {
+      this.frequencyModel = "document";
+  }
+  public ChiSquareTransformLearner(String model)
+  {
+    this.frequencyModel = model;
+  }
+
+  /** only accepts binary schemas */
+  public void setSchema(ExampleSchema schema)
+  {
+    if (!ExampleSchema.BINARY_EXAMPLE_SCHEMA.equals(schema))
+    {
+      throw new IllegalStateException("can only learn binary example data");
+    }
+  }
+
+  /** Examine data, build an instance transformer */
+  public InstanceTransform batchTrain(Dataset dataset)
+  {
+    ChiSquareInstanceTransform filter = new ChiSquareInstanceTransform();
+    BasicFeatureIndex index = new BasicFeatureIndex(dataset);
+
+    if (frequencyModel.equals("document"))
+    {
+      int totalPos = index.size(ExampleSchema.POS_CLASS_NAME);
+      int totalNeg = index.size(ExampleSchema.NEG_CLASS_NAME);
+      if((totalPos+totalNeg)!=(dataset.size())){
+          throw new IllegalStateException("ERROR - Dataset size and index size do not match");
+      }
+      
+      for (Feature.Looper i=index.featureIterator(); i.hasNext(); )
+      {
+        Feature f = i.nextFeature();
+        int a = index.size(f,ExampleSchema.POS_CLASS_NAME);
+        int b = index.size(f,ExampleSchema.NEG_CLASS_NAME);
+        int c = totalPos - a;
+        int d = totalNeg - b;
+        
+        ContingencyTable ct = new ContingencyTable(a,b,c,d);
+        double chiScore = ct.getChiSquared();
+//        double chiScore = ct.getPMutualInfo();
+//        double chiScore = ct.getCompensatedPMutualInfo(count(a));
+        filter.addFeature( chiScore,f );
+      }
+    }
+
+    else if(frequencyModel.equals("word"))//not implemented yet
+    {
+      System.out.println( "warning: "+ frequencyModel +" not implemented yet!" );
+      System.exit(1);
+    }
+    else
+    {
+      System.out.println( "warning: "+ frequencyModel +" is an unknown model for frequency!" );
+      System.exit(1);
+    }
+
+    return filter;
+  }
+
+  static public void main(String[] args)
+  {
+    Dataset dataset = SampleDatasets.sampleData("toy",false);
+    System.out.println( "old data:\n" + dataset );
+    ChiSquareTransformLearner learner = new ChiSquareTransformLearner();
+    ChiSquareInstanceTransform filter = (ChiSquareInstanceTransform)learner.batchTrain( dataset );
+    filter.setNumberOfFeatures(10);
+    dataset = filter.transform( dataset );
+    System.out.println( "new data:\n" + dataset );
+    System.out.println( "\n\n\n " + filter.toString(8));
+    
+  }
+}
