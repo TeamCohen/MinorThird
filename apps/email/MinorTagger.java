b6a70be47347d456313a584632ca55054892042f
Cameron Williams <cmw2@cs.cmu.edu>
2005-04-22 17:55:49 +0000

    New MinorTagger.java and MinorTaggerClient.pl

--- apps/email/MinorTagger.java
+++ apps/email/MinorTagger.java
@@ -10,33 +10,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Description:
- * An echo-like server that labels popular entities using XML tags in an input text
- *
- * Instruction:
- *
- * VERY IMPORTANT: Make sure you comment out the line
- * require 'pos';
- * in "np.mixup" because this server already does POS tagging for you!
- * If you don't want to change the original mixup file, you can copy "np.mixup"
- * to MinorTagger's directory and then comment out the above line.
- *
- * To use Minorthird's createXMLmarkup function instead of mine, uncomment the line:
- * tagged = labelsLoader.createXMLmarkup(tempFile.getName(), labels);
- * and comment out the line:
- * tagged = createXML(in, labelsLoader.saveTypesAsXML(labels));
- * in the "tag" function. To switch back, do the opposite.
- *
- * After connecting to the server, the client can send in any text. The server will
- * buffer the incoming text until it detects that a line is ended with TWO consecutive
- * dollar signs ($$). The server will then process the text in the buffer and echo back
- * the resulting text without disconnecting the client. However, if a line is ended
- * with THREE consecutive dollar signs ($$$), then after the server echos back the
- * resulting text, it will disconnect the connection automatically.
- *
- * @author Richard Wang <rcwang@cmu.edu>
- */
+
 public class MinorTagger {
 
 	private static JMontyLingua montyLingua;
@@ -73,108 +47,9 @@ public class MinorTagger {
 		}
 	}
 
-	public static String POSTag (String in) {
-		String workingString = in.replaceAll("<[^<>]+>", "");
-		String strToken = null;
-		String pos = null;
-		String word = null;
-		StringBuffer XMLTagged = new StringBuffer();
-		int sep = 0;
-		int endPointer = 0;
-
-		String tagged = montyLingua.tag_text(workingString);
-		if (DEBUG) System.out.println("Tagged: " + tagged);
-		StringTokenizer tokeTagged = new StringTokenizer(tagged, "\n ", false);
-
-		while (tokeTagged.hasMoreTokens()) {
-			strToken = tokeTagged.nextToken();
-			sep = strToken.lastIndexOf("/");
-			word = strToken.substring(0, sep);
-			if (DEBUG) System.out.println("word: " + word);
-			pos = strToken.substring(sep + 1);
-			if (DEBUG) System.out.println("POS: " + pos);
-			if (pos.endsWith("$"))
-				pos = pos.replace('$', 'S');
-			// look for alternative word
-			if (!workingString.trim().toLowerCase().startsWith(word.toLowerCase()))
-				word = findAlternative(workingString, word);
-			// ensure case are the same
-			if (workingString.trim().toLowerCase().startsWith(word.toLowerCase()))
-				word = workingString.trim().substring(0, word.length());
-			workingString = substFirst(workingString, word, "<" + pos + ">" + word + "</" + pos + ">", false);
-			if (DEBUG) System.out.println("WorkingString: " + workingString);
-			endPointer = workingString.lastIndexOf("</" + pos + ">") + ("</" + pos + ">").length();
-			if (DEBUG) System.out.println("EndPointer: " + endPointer);
-			XMLTagged.append(workingString.substring(0, endPointer));
-			workingString = workingString.substring(endPointer); //in_ptr, working.length()
-		}
-		return XMLTagged.toString();
-	}
-
-	private static String substFirst (String in, String find, String newStr, boolean case_sensitive) {
-		char[] working = in.toCharArray();
-		StringBuffer sb = new StringBuffer();
-		// int startindex =  in.indexOf(find);
-		int startindex = 0;
-		if (case_sensitive)
-			startindex = in.indexOf(find);
-		else
-			startindex = (in.toLowerCase()).indexOf(find.toLowerCase());
-		if (startindex < 0) return in;
-		int currindex = 0;
-		for (int i = currindex; i < startindex; i++)
-			sb.append(working[i]);
-		currindex = startindex;
-		sb.append(newStr);
-		currindex += find.length();
-		for (int i = currindex; i < working.length; i++)
-			sb.append(working[i]);
-		return sb.toString();
-	}
-
-	private static String findAlternative (String in, String word) {
-		String workingString = in.trim().toLowerCase();
-		String workingWord = word.toLowerCase();
-		String find = word;
-		if (workingWord.equals("not")) {
-			if (workingString.startsWith("n't"))
-				find = "n't";
-			else if (workingString.startsWith("'t"))
-				find = "'t";
-		} else if (workingWord.equals("-")) {
-			if (workingString.startsWith("/"))
-				find = "/";
-		} else if (workingWord.equals("want")) {
-			if (workingString.startsWith("wan"))
-				find = "wan";
-		} else if (workingWord.equals("going")) {
-			if (workingString.startsWith("gon"))
-				find = "gon";
-		} else if (workingWord.equals("have")) {
-			if (workingString.startsWith("haf"))
-				find = "haf";
-		} else if (workingWord.equals("to")) {
-			if (workingString.startsWith("na"))
-				find = "na";
-			else if (workingString.startsWith("ta"))
-				find = "ta";
-		} else if (workingWord.equals("give")) {
-			if (workingString.startsWith("gim"))
-				find = "gim";
-		} else if (workingWord.equals("let")) {
-			if (workingString.startsWith("lem"))
-				find = "lem";
-		} else {
-			System.err.println("POS Tagging error!");
-			System.err.println("Working string:" + in);
-			System.err.println("Current word:" + word);
-		}
-		return find;
-	}
-
 	private static String tag (String in) throws Exception {
 		if (in.replaceAll("\\s+", "").length() == 0) return "";
-		String tagged = POSTag(in);
+		String tagged = in;
 
 		// load text base
 		TextBaseLoader baseLoader = new TextBaseLoader();
@@ -195,13 +70,7 @@ public class MinorTagger {
 		// Minorthird's version of marking up XML labels
 		tagged = labelsLoader.createXMLmarkup(tempFile.getName(), labels);
 
-		// My own version of marking up XML labels; doesn't check for overlapping spans
-		//tagged = createXML(in, labelsLoader.saveTypesAsXML(labels));
-
-		String keepXML[] = {
-			"CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR", "JJS", "LS", "MD", "NN", "NNS", "NNP", "NNPS", "PDT", "POS", "PRP", "PRPS", "RB", "RBR", "RBS", "RP", "TO", "UH", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ", "WDT", "WP", "WPS", "WRB",
-			"S", "NP", "name", "extracted_date", "extracted_time"
-		};
+		String keepXML[] = {"S", "NP", "Name", "extracted_date", "extracted_time"};
 		tagged = filterXML(tagged, keepXML);
 
 		// fix the problem so that end-tag doesn't stick to the next begin-tag.
@@ -229,58 +98,6 @@ public class MinorTagger {
 			tagged = tagged.replaceAll("</?\\Q" + del + "\\E>", "");
 		}
 		return tagged;
-	}
-
-	private static String createXML (String in, String XMLIndex) {
-		HashMap hash = new HashMap();
-		String temp;
-		ArrayList spanList = new ArrayList();
-		StringBuffer buf = new StringBuffer(in);
-		Matcher m = Pattern.compile("[\r\n]  <(\\S+) lo=(\\d+) hi=(\\d+)>").matcher(XMLIndex);
-
-		while (m.find()) {
-			ArrayList span = new ArrayList();
-			span.add(m.group(1));
-			span.add(new Integer(m.group(2)));
-			span.add(new Integer(m.group(3)));
-			spanList.add(span);
-		}
-
-		Collections.sort(spanList, new Comparator() {
-			public int compare (Object o1, Object o2) {
-				Integer lo1 = (Integer) ((ArrayList) o1).get(1);
-				Integer lo2 = (Integer) ((ArrayList) o2).get(1);
-				Integer hi1 = (Integer) ((ArrayList) o1).get(2);
-				Integer hi2 = (Integer) ((ArrayList) o2).get(2);
-				if (lo1.compareTo(lo2) == 0)
-					return hi2.compareTo(hi1);
-				else return lo1.compareTo(lo2);
-			}
-		});
-
-		for (Iterator i = spanList.iterator(); i.hasNext(); ) {
-			ArrayList span = (ArrayList) i.next();
-			String xml = (String) span.get(0);
-			String start = "<" + xml + ">";
-			String end = "</" + xml + ">";
-			Integer lo = (Integer) span.get(1);
-			Integer hi = (Integer) span.get(2);
-			temp = (String) hash.get(lo);
-			hash.put(lo, ((temp == null) ? "" : temp) + start);
-			temp = (String) hash.get(hi);
-			hash.put(hi, end + ((temp == null) ? "" : temp));
-		}
-
-		ArrayList al = new ArrayList(hash.keySet());
-		Collections.sort(al);
-		int counter = 0;
-		for (Iterator i = al.iterator(); i.hasNext();) {
-			Integer index = (Integer) i.next();
-			String tags = (String) hash.get(index);
-			buf.insert(index.intValue() + counter, tags);
-			counter += tags.length();
-		}
-		return buf.toString();
 	}
 
 	// create a temp file from a string
@@ -330,7 +147,6 @@ public class MinorTagger {
 					buf.append(line + "\n");
 					if (end_close || end_continue) {
 						os.println(tag(buf.toString()));
-						//os.println("|");
 						buf.setLength(0);
 					}
 					if (end_close) {
--- apps/email/MinorTaggerClient.pl
+++ apps/email/MinorTaggerClient.pl
@@ -1,112 +1,56 @@
 #!/usr/local/bin/perl
 
 use IO::Socket;
-use FileHandle;
 use Getopt::Std;
 
-getopts('vf:qF:o:');
+getopts('o:');
 
-if ($opt_f) {
-    $file = $opt_f;
-} 
-
-if ($opt_F) {
-    $file = $opt_F;
-}
 if ($opt_o) {
   $out = $opt_o;
+  open(OUT, ">./$opt_o");
 }
 
-$quote = $opt_q;
-
-if ($#ARGV != 1) {
-
-    die "Usage: [-F file] [-o output file]  host port 
-\nREMINDER: All input file must end with: \$\$\$ \nHost Names: roxie and velmak \nBoth hosts are running on port 9998 \n";
-
-}
-
-$| = 1;
-
-$remote_host = $ARGV[0];
+$remote_host = shift;
+$remote_port = shift;
+$file = shift;
 
 if ($remote_host eq "raff") {
     $remote_host = "128.2.205.132";
-} elsif ($remote_host eq "cammie"){
-    $remote_host = "128.2.191.175";
   } elsif ($remote_host eq "roxie") {
     $remote_host = "128.2.209.54";
   } elsif ($remote_host eq "velmak") {
     $remote_host = "128.2.209.57";
   }
 
-$remote_port = $ARGV[1];
-
-$socket = new IO::Socket::INET (PeerAddr => $remote_host,
-                                PeerPort => $remote_port,
-                                Proto => "tcp",
-                                Type => SOCK_STREAM)
-    or die "Can't connect to $remote_host:$remote_port : $!\nREMINDER: Both roxie and velmak are running on port 9998 \n";
-
-#$socket = IO::Socket::INET->new("$remote_host:$remote_port") 
-#    or die "Can't connect to $remote_host:$remote_port : $!\n";
-
-*STDIN->autoflush(1);
-
-print "REMINDER: \nTo Use MinorTaggerClient your file must end with: \$\$\$ \nHost Names: roxie and velmak \nBoth hosts are running on port 9998 \n";
-
-if ($opt_f || $opt_F) {
-    $fh = new FileHandle "$file";
-} else {
-    $fh = *STDIN;
+unless ($file) {
+  die "usage: $0 -o output_file host port file \n";
 }
 
-if ($opt_f ||$opt_F) {
-    @all = <$fh>;
-    $fileText = join(/\n/, @all);
-    $exit = "\$\$\$ \n\n";
-    $all = $fileText.$exit;
-    $trial = "Hello my name is Cammie Williams. \$\$\$ \n\n";
-    sendMessage($socket, $all);
-    readMessage($socket);
+$socket = new 
+  IO::Socket::INET (PeerAddr => $remote_host,
+		    PeerPort => $remote_port,
+		    Proto => 'tcp',
+		    Type => SOCK_STREAM)
+  or die "Can't connect to $remote_host:$remote_port : $!";
+
+$msg = '';
+open(F,"<$file") || die "Can't open file $file: $!";
+while ($line = <F>) {
+  $msg .= $line;
 }
+$msg =~ s/^\s+//;
+$msg =~ s/\s+$//;
+#special 'sentinal' line to indicate the end of a message
+$msg .= '$$$' . "\n";
 
-if ((!$file) || $opt_v) {
-    print "$remote_host:$remote_port> ";
+#print "message was '",$msg,"'\n";
+
+#send sentinal-terminated message to the server
+print $socket $msg;
+
+#get the result back
+while ($line = <$socket>) {
+  if($opt_o){
+    print OUT $line;
+  } else {print $line;}
 }
-
-sub readMessage {
-    my ($socket) = @_;
-
-    #Opens out file if option o is defined
-    if ($opt_o){
-      open(OUT, ">./$opt_o");
-    } else {
-      $isOut = 1;
-    }
-
-    $socket->recv($text,1);
-    $recievedText = $text;
-    while($text ne '')
-      {
-	$socket->recv($text,16);
-	$recievedText = $recievedText.$text;
-      }
-
-    #prints to console or to OUT file
-    if($isOut == 1) {
-      print "$recievedText";
-    }
-    else {
-      print OUT "$recievedText";
-    }
-    close(OUT);
-}
-
-sub sendMessage {
-    my ($socket, $message) = @_;
-    chomp $message;
-    my $len = length $message;
-    print $socket "$len $message";
-}
-
