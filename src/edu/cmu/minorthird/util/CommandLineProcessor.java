06d276fa66a2a8513fcdc2ab3b104de0eec0aba1
William W. Cohen <wcohen@cs.cmu.edu>
2006-07-03 14:46:32 +0000

    better documentation

--- src/edu/cmu/minorthird/util/CommandLineProcessor.java
+++ src/edu/cmu/minorthird/util/CommandLineProcessor.java
@@ -10,53 +10,59 @@ import java.util.*;
  */
 public interface CommandLineProcessor 
 {
-	/** Loop thru the command-line arguments.  Option pairs like "-foo
-	 * bar" should cause a call to x.foo("bar"), where x is this
-	 * CommandLineProcessor.  Options like "-gorp" cause a call to
-	 * x.gorp().  Options without corresponding methods are considered
-	 * errors.  If an error occurs, then an IllegalArgumentException is
-	 * thrown.
-	 *
-	 * <p>If function like -foo happens to return a non-null
-	 * CommandLineProcessor, that processor is should be invoked on the
-	 * arguments immediately after foo.
-	 *
-	 * <p>The optional "-config FILE" should cause a Properties object
-	 * to be loaded from FILE, and the property/value pairs in the
-	 * Properties object to be treated as if they were option/argument
-	 * pairs.  The order of lines in the property file may not be
-	 * preserved, however, and duplicated keys will be ignored.
-	 *
-	 * <p>For instance, calling -config FILE for a FILE containing
-	 *<code><pre>
-	 *baz=
-	 *foo=bar
-	 *</pre></code>
-	 * would be the same as the command options <code>-foo bar -baz</code>
-	 * or possibly <code>-baz -foo bar</code>
-	 * 
-	 */
-	public void processArguments(String[] args);
+    /** Loop thru the command-line arguments.  Option pairs like "-foo
+     * bar" should cause a call to x.foo("bar"), where x is this
+     * CommandLineProcessor.  Options like "-gorp" cause a call to
+     * x.gorp().  Options without corresponding methods are considered
+     * errors.  If an error occurs, then an IllegalArgumentException is
+     * thrown.
+     *
+     * <p>If function like -foo happens to return a non-null
+     * CommandLineProcessor, that processor is invoked on the
+     * arguments immediately after foo.  This allows one
+     * CommandLineProcessor to pass command-line options to a second
+     * one. For instance, if x and y are CommandLineProcessors, x has
+     * an method colorOpts() that returns y, and y has methods
+     * background(String s) and foreground(String t) then the command
+     * line sequence <code>-colorOpts -background blue -foreground
+     * red</code> would call the expected methods of y.
+     *
+     * <p>The optional "-config FILE" should cause a Properties object
+     * to be loaded from FILE, and the property/value pairs in the
+     * Properties object to be treated as if they were option/argument
+     * pairs.  The order of lines in the property file may not be
+     * preserved, however, and duplicated keys will be ignored.
+     *
+     * <p>For instance, calling -config FILE for a FILE containing
+     *<code><pre>
+     *baz=
+     *foo=bar
+     *</pre></code>
+     * would be the same as the command options <code>-foo bar -baz</code>
+     * or possibly <code>-baz -foo bar</code>
+     * 
+     */
+    public void processArguments(String[] args);
 
-	/** Try to consume a the command-line argument at position i.
-	 * Return the number of arguments successfully consumed.
-	 */
-	public int consumeArguments(String[] args,int startPos);
+    /** Try to consume a the command-line argument at position i.
+     * Return the number of arguments successfully consumed.
+     */
+    public int consumeArguments(String[] args,int startPos);
 
-	/** Prints errorMessage and then calls usage(). 
-	 */
-	public void usage(String errorMessage); 
+    /** Prints errorMessage and then calls usage(). 
+     */
+    public void usage(String errorMessage); 
 
-	/** Give usage() information. 
-	 */
-	public void usage();
+    /** Give usage() information. 
+     */
+    public void usage();
 
-	/** Interface for objects that can be configured with command-line arguments.
-	 * Configuration for x is done by calling <code>x.getCLP().processArgs(ags).</code>
-	 */
-	public interface Configurable 
-	{
-		/** Produce a command-line processor that configures this object. */
-		public CommandLineProcessor getCLP();
-	}
+    /** Interface for objects that can be configured with command-line arguments.
+     * Configuration for x is done by calling <code>x.getCLP().processArgs(ags).</code>
+     */
+    public interface Configurable 
+    {
+        /** Produce a command-line processor that configures this object. */
+        public CommandLineProcessor getCLP();
+    }
 }
