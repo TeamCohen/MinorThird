9e26addf797c733513c5a3f0d2164b863131e2e5
Quinten C. Mercer III <qcm@cs.cmu.edu>
2006-10-23 20:39:14 +0000

    Added comments from my previous relase to the version and version log

--- src/edu/cmu/minorthird/util/Version.java
+++ src/edu/cmu/minorthird/util/Version.java
@@ -3,7 +3,7 @@ package edu.cmu.minorthird.util;
 /** Stores the version number */
 public class Version {
 
-	private static String version = "Version 9.6.4.19";
+	private static String version = "Version 11.6.10.23";
 	
 
     public static String getVersion() {
