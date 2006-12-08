6afc3d7c6b75397ad70214301a571d4e12b188a7
Quinten C. Mercer III <qcm@cs.cmu.edu>
2006-12-08 05:05:30 +0000

    I added the version log to the javadocs of this class

--- src/edu/cmu/minorthird/util/Version.java
+++ src/edu/cmu/minorthird/util/Version.java
@@ -1,6 +1,108 @@
 package edu.cmu.minorthird.util;
 
-/** Stores the version number */
+/** Stores the version number as: <br>
+ * <br>
+ * Version # Format: Version#.Year.Month.Day <br>
+ * <br>
+ * Version History:
+ * <hr>
+ * <table border="0">
+ *   <tr valign=top><td align="right">1.5.7.15</td> 
+ *     <td align="left">
+ *       - First Version to be checked in. <br>
+ *       - Recently added Mixup tokenization extras 
+ *   </td></tr>
+ *   <tr valign=top><td align="right">2.5.8.9</td>
+ *     <td align="left">-  Added multi label classification to text package
+ *   </td></tr> 
+ *   <tr valign=top><td align="right">3.5.8.18</td>
+ *     <td align="left">-  New Explanation Facilities
+ *   </td></tr>
+ *   <tr valign=top><td align="right">3.2.5.9.15</td>
+ *     <td align="left">Fixed Bugs:<br>
+ *                 - createSpanProp <br>
+ *                 - MaxEntLearner(CRFLearner)log space option: trainer ll (for large german dataset) <br>
+ *                 - CascadingBinaryLearner macKappa changed from 0.0 t0 -1.0 <br>
+ *                 - MultiClassifierAnnotator outputs _predicted_spanProp <br>
+ *   </td></tr> 
+ *   <tr valign=top><td align="right">4.5.10.3</td>
+ *     <td align="left">- NEW GUI<br>
+ *                 -  other option for classifyCommandLineUtil<br>
+ *                 -  Fixed saving bug in classify package<br>
+ *   </td></tr>
+ *   <tr valign=top><td align="right">5.5.10.17</td>
+ *     <td align="left">-  Fixed bug in GUI (PE doesn't pop up when TextField is entered) <br>
+ *                 -  Added TransformingMultiLearner and PredictedClassTransform for adding predicted class
+ *                    names as features for multi label classification
+ *   </td></tr>
+ *   <tr valign=top><td align="right">5.2.5.10.27</td>
+ *     <td align="left">- a few gui fixes: help button fixes, showTestDetails automatic, DebugMixup, 
+ *                        and EditParams added to Selectable Types<br>
+ *                 - Saving Extractor bug fixed<br>
+ *                 - classify command line to gui -learner option consistency fixed<br>
+ *   </td></tr>
+ *   <tr valign=top><td align="right">6.5.11.30</td>
+ *     <td align="left">- More gui updates:  shorted class names, links to tutorials, links to javadocs, more help buttons<br>
+ *                 - more parameter bunches added to selectable types<br>
+ *                 - Documentation added: Index and tutorial<br>
+ *                 - Fixed cross option for TrainTestMultiClassifier - Tester.multiEvaluate, MultiCrossValidatedDataset<br>
+ *   </td></tr>
+ *   <tr valign=top><td align="right">6.2.5.12.8</td>
+ *     <td align="left">- Fixed MultiClassifier cross testing implementation<br>
+ *                 - Fixed MultiClassifier explanation facilities<br>
+ *                 - Added and updated documentation<br>
+ *                 - Fixed SpanTypeTextBase to extract more than one span Type per document</br>
+ *    </td></tr>
+ *    <tr valign=top><td align="right">7.6.1.11</td>
+ *      <td align="left">- added demos/MyFE.java as an example<br>
+ *                 - modified CommandLineUtil.newObjectFromBSH to allow bsh.source(file)<br>
+ *                 - added PreprocessTextForClassifier, PreprocessTextForExtractor<br>
+ *                 - added text.learn.BeginContinueOutsideReduction<br>
+ *                 - updated selectableTypes as appropriate<br>
+ *                 - fixed buggy implementation of span.getLoChar(), span.getHiChar()<br>
+ *    </td></tr>
+ *    <tr valign=top><td align="right">7.6.1.31</td>
+ *      <td align="left">- added main to SpanDifference<br>
+ *                 - added mixup command 'annotateWith FILE'<br>
+ *    </td></tr>
+ *    <tr valign=top><td align="right">8.6.2.13</td>
+ *      <td align="left">- Changed createSpanProp to implement Mixup<br>
+ *                 - fixed semiCRF saving bug<br>
+ *                 - fixed DecisionTree Print<br>
+ *                 - Updated MinorTagger and MinorTaggerClient to output labels format and run on velmak<br>
+ *                 - Cleaned up commandLineUtil<br>
+ *                 - Fixed bug in RefUtils<br>
+ *    </td></tr>
+ *    <tr valign=top><td align="right">8.6.3.15</td>
+ *      <td align="left">- cleaned up output in ProgressCounter<br>
+ *                 - fixed some bugs in TextBaseLoader, for reading in text with XML labels<br>
+ *                 - added ProgressCounter to TextBaseLoader for loading directories<br>
+ *                 - made OneVsAllLearner work for Online learners<br>
+ *    </td></tr>
+ *    <tr valign=top><td align="right">9.6.4.19</td>
+ *      <td align="left">- NEW classify GUI<br>
+ *                 - fixed bug in CRFLearner<br>
+ *                 - New LevelManager and TexBaseMapper for handling multiple levels<br>
+ *                 - importLabels taken out of TextBase<br>
+ *                 - Encapsulated Annotator Loader fixed<br>
+ *                 - Updated Documentation<br>
+ *                 - Cammie's Last Day!<br>
+ *    </td></tr>
+ *    <tr valign=top><td align="right">10.6.10.12</td>
+ *      <td align="left">- Added confidence calculation capability to the SVM learner<br>
+ *                 - Fixed encapsulated annotators so that they can require other encapsulated annotators.<br>
+ *    </td></tr>
+ *    <tr valign=top><td align="right">10.6.10.23</td>
+ *      <td align="left">- Fixed errors in MixupPrograms handling of regular expressions.
+ *    </td></tr>
+ *    <tr valign=top><td align="right">11.6.11.21</td>
+ *      <td align="left">- Initial version of extractor-confidence computation
+ *    </td></tr>
+ *    <tr valign=top><td align="right">11.6.11.22</td>
+ *      <td align="left"> - Better version of extractor-confidence computation, with BeamSearch modified appropriately
+ *    </td></tr>
+ * </table>
+ */
 public class Version {
 
     private static String version = "Version 11.6.11.22";
