f1b3614096a45a7adba203c47370c049181837d0
Chia-chi Chuang <chiachi@cs.cmu.edu>
2007-10-08 20:57:12 +0000

    Added getFeature(id).

--- src/edu/cmu/minorthird/classify/FeatureIdFactory.java
+++ src/edu/cmu/minorthird/classify/FeatureIdFactory.java
@@ -6,31 +6,41 @@ import gnu.trove.TObjectIntHashMap;
 
 import java.io.Serializable;
 import java.util.*;
+import java.util.Hashtable;
 
 public class FeatureIdFactory implements java.io.Serializable
 {
+    private Hashtable featuresHT = new Hashtable();
+	
     private TObjectIntHashMap featureIds = new TObjectIntHashMap();
-
-		public FeatureIdFactory (Dataset dataset)
-		{
-			int nextID = 1;
+    
+    public FeatureIdFactory (Dataset dataset)
+    {
+	int nextID = 1;
         for (Example.Looper i = dataset.iterator(); i.hasNext();)
         {
-            Example e = i.nextExample();
-            for (Feature.Looper j = e.featureIterator(); j.hasNext();)
+	    Example e = i.nextExample();
+	    for (Feature.Looper j = e.featureIterator(); j.hasNext();)
             {
-                Feature f = j.nextFeature();
-								if (!featureIds.containsKey(f))
-									featureIds.put(f,  nextID++);
-            }
-				}
+		Feature f = j.nextFeature();
+		if (!featureIds.containsKey(f)) {
+		    featuresHT.put(nextID, f);
+		    featureIds.put(f,  nextID++);
 		}
+	     }
+         }
+    }
+	
+    public Feature getFeature(int featureId)
+    {
+	return (Feature)featuresHT.get(featureId);
+    }
 
     public int getID(Feature feature)
     {
-      int id = featureIds.get(feature);
-      if (id < 0)
-				return featureIds.size()+1;
-      return id;
+	int id = featureIds.get(feature);
+	if (id < 0)
+	    return featureIds.size()+1;
+	return id;
     }
 }
