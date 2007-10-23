8b6bf8dfebe580ec49e6df5a57c988610900f9fa
Frank Lin <frankl@.cmu.edu>
2007-10-23 07:16:42 +0000

    *** empty log message ***

--- src/edu/cmu/minorthird/text/AnnotatorLoader.java
+++ src/edu/cmu/minorthird/text/AnnotatorLoader.java
@@ -105,7 +105,6 @@ public abstract class AnnotatorLoader{
 
 			// If all else fails attempt to load the annotation type as a class
 			log.debug("trying as class "+annotationType);
-			log.info(" => "+annotationType);
 			return findNativeAnnotatorFromString(annotationType);
 		}
 
