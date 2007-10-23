8b01ceacc193c521fbaf03ec2bf779fa5fd25dae
Frank Lin <frankl@.cmu.edu>
2007-10-23 01:31:21 +0000

    added generic types

--- src/edu/cmu/minorthird/classify/CascadingBinaryLearner.java
+++ src/edu/cmu/minorthird/classify/CascadingBinaryLearner.java
@@ -11,124 +11,123 @@ import java.util.*;
  * @author Cameron Williams
  */
 
-public class CascadingBinaryLearner extends OneVsAllLearner
-{
+public class CascadingBinaryLearner extends OneVsAllLearner{
 
-    public String[] sortedClassNames; 
-    private ArrayList data = null;
-    private ArrayList eval = null;
+	public String[] sortedClassNames;
 
-    public CascadingBinaryLearner()
-    {
-	super();
-    }
+	private List<Dataset> data=null;
+	private List<Evaluation> eval=null;
 
-    /** 
-     * @deprecated use CascadingBinaryLearner(BatchClassifierLearner learner)
-     * @param learnerFactory a ClassifierLearnerFactory which should produce a BinaryClassifier with each call.
-     */
-    public CascadingBinaryLearner(ClassifierLearnerFactory learnerFactory)
-    {
-	super(learnerFactory);
-    }
-    public CascadingBinaryLearner(String l) {
-	super(l);	
-    }
-    public CascadingBinaryLearner(BatchClassifierLearner learner) {
-	this.learner = learner;
-	this.learnerName = learner.toString();
-	learnerFactory = new ClassifierLearnerFactory(learnerName);
-	
-    }
-    public void setSchema(ExampleSchema schema)
-    {
-	this.schema = schema;
-	innerLearner = new ArrayList();
-	data = new ArrayList();
-	//for (int i=0; i<innerLearner.size(); i++) {
-	for(int i=0; i<schema.getNumberOfClasses(); i++) {
-	    innerLearner.add(((BatchClassifierLearner)learner).copy());	   
-	    ((ClassifierLearner)(innerLearner.get(i))).setSchema( ExampleSchema.BINARY_EXAMPLE_SCHEMA );
-	    data.add(new BasicDataset());
-	}
-    }
-    private void createRankings() {
-	Splitter splitter = new CrossValSplitter(9);
-	eval = new ArrayList();
-	for (int i=0; i<innerLearner.size(); i++) {	    
-	    Evaluation evaluation = Tester.evaluate(((ClassifierLearner)(innerLearner.get(i))),((Dataset)(data.get(i))),splitter);
-	    eval.add(evaluation);
-	}
-    }
-    private void sortLearners() {
-	ArrayList unsortedLearners = new ArrayList();
-	String[] classNames = schema.validClassNames();
-	ArrayList unsortedClassNames = new ArrayList();
-	sortedClassNames = new String[schema.getNumberOfClasses()];
-	for (int i=0; i<innerLearner.size(); i++) {
-	    unsortedLearners.add((BatchClassifierLearner)innerLearner.get(i));
-	    unsortedClassNames.add(classNames[i]);
+	public CascadingBinaryLearner(){
+		super();
 	}
 
-	//clear list so that it can be reconstructed in sorted order
-	innerLearner.clear();
+	/** 
+	 * @deprecated use CascadingBinaryLearner(BatchClassifierLearner learner)
+	 * @param learnerFactory a ClassifierLearnerFactory which should produce a BinaryClassifier with each call.
+	 */
+	public CascadingBinaryLearner(ClassifierLearnerFactory learnerFactory){
+		super(learnerFactory);
+	}
 
-	int position = 0;
-	while(!unsortedLearners.isEmpty()) {
-	    double maxKappa = -10.0;
-	    int learnerIndex = -1;
-	    //find learner with max positive examples
-	    for(int j=0; j<unsortedLearners.size(); j++) {
-		try {
-		    //BatchClassifierLearner learner = ((BatchClassifierLearner)unsortedLearners.get(j));
-		    Evaluation evaluation = (Evaluation)eval.get(j);
-		    double kappa = evaluation.kappa();
-		    if(kappa>=maxKappa) {
-			maxKappa = kappa;
-			learnerIndex = j;
-		    }
-		} catch (Exception e) {
-		    e.printStackTrace();
+	public CascadingBinaryLearner(String l){
+		super(l);
+	}
+
+	public CascadingBinaryLearner(BatchClassifierLearner learner){
+		this.learner=learner;
+		this.learnerName=learner.toString();
+		learnerFactory=new ClassifierLearnerFactory(learnerName);
+	}
+
+	public void setSchema(ExampleSchema schema){
+		this.schema=schema;
+		innerLearner=new ArrayList<ClassifierLearner>();
+		data=new ArrayList<Dataset>();
+		//for (int i=0; i<innerLearner.size(); i++) {
+		for(int i=0;i<schema.getNumberOfClasses();i++){
+			innerLearner.add(learner.copy());
+			innerLearner.get(i).setSchema(ExampleSchema.BINARY_EXAMPLE_SCHEMA);
+			data.add(new BasicDataset());
 		}
-	    }
-	    
-	    //add learner to sortedLearners
-	    ClassifierLearner learner = (ClassifierLearner)unsortedLearners.remove(learnerIndex);
-	    innerLearner.add(learner);
-
-	    String className = (String)unsortedClassNames.remove(learnerIndex);
-	    sortedClassNames[position] = className;
-	    position++;
 	}
-       
-    }
 
-    public void addExample(Example answeredQuery)
-    {
-	int classIndex = schema.getClassIndex( answeredQuery.getLabel().bestClassName() );
-	for (int i=0; i<innerLearner.size(); i++) {
-	    ClassLabel label = classIndex==i ? ClassLabel.positiveLabel(1.0) : ClassLabel.negativeLabel(-1.0);
-	    Example ex = new Example( answeredQuery.asInstance(), label ); 
-	    ((ClassifierLearner)(innerLearner.get(i))).addExample(ex);
-	    ((Dataset)(data.get(i))).add(ex);
+	private void createRankings(){
+		// why 9?
+		Splitter splitter=new CrossValSplitter(9);
+		eval=new ArrayList<Evaluation>();
+		for(int i=0;i<innerLearner.size();i++){
+			Evaluation evaluation=Tester.evaluate(innerLearner.get(i),data.get(i),splitter);
+			eval.add(evaluation);
+		}
 	}
-    }
 
-    public void completeTraining()
-    {
-	for (int i=0; i<innerLearner.size(); i++) {
-	    ((ClassifierLearner)innerLearner.get(i)).completeTraining();
-	}
-	createRankings();
-	sortLearners();
-    }
+	private void sortLearners(){
+		List<BatchClassifierLearner> unsortedLearners=new ArrayList<BatchClassifierLearner>();
+		String[] classNames=schema.validClassNames();
+		List<String> unsortedClassNames=new ArrayList<String>();
+		sortedClassNames=new String[schema.getNumberOfClasses()];
+		for(int i=0;i<innerLearner.size();i++){
+			unsortedLearners.add((BatchClassifierLearner)innerLearner.get(i));
+			unsortedClassNames.add(classNames[i]);
+		}
 
-    public Classifier getClassifier()
-    {
-	Classifier[] classifiers = new Classifier[ innerLearner.size() ];
-	for (int i=0; i<innerLearner.size(); i++) {
-	    classifiers[i] = ((ClassifierLearner)(innerLearner.get(i))).getClassifier();
+		//clear list so that it can be reconstructed in sorted order
+		innerLearner.clear();
+
+		int position=0;
+		while(!unsortedLearners.isEmpty()){
+			double maxKappa=-10.0;
+			int learnerIndex=-1;
+			//find learner with max positive examples
+			for(int j=0;j<unsortedLearners.size();j++){
+				try{
+					//BatchClassifierLearner learner = ((BatchClassifierLearner)unsortedLearners.get(j));
+					Evaluation evaluation=(Evaluation)eval.get(j);
+					double kappa=evaluation.kappa();
+					if(kappa>=maxKappa){
+						maxKappa=kappa;
+						learnerIndex=j;
+					}
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+
+			//add learner to sortedLearners
+			ClassifierLearner learner=unsortedLearners.remove(learnerIndex);
+			innerLearner.add(learner);
+
+			String className=unsortedClassNames.remove(learnerIndex);
+			sortedClassNames[position]=className;
+			position++;
+		}
+
 	}
-	return new OneVsAllClassifier( sortedClassNames, classifiers );
-    }
+
+	public void addExample(Example answeredQuery){
+		int classIndex=schema.getClassIndex(answeredQuery.getLabel().bestClassName());
+		for(int i=0;i<innerLearner.size();i++){
+			ClassLabel label=classIndex==i?ClassLabel.positiveLabel(1.0):ClassLabel.negativeLabel(-1.0);
+			Example example=new Example(answeredQuery.asInstance(),label);
+			innerLearner.get(i).addExample(example);
+			data.get(i).add(example);
+		}
+	}
+
+	public void completeTraining(){
+		for(int i=0;i<innerLearner.size();i++){
+			innerLearner.get(i).completeTraining();
+		}
+		createRankings();
+		sortLearners();
+	}
+
+	public Classifier getClassifier(){
+		Classifier[] classifiers=new Classifier[innerLearner.size()];
+		for(int i=0;i<innerLearner.size();i++){
+			classifiers[i]=innerLearner.get(i).getClassifier();
+		}
+		return new OneVsAllClassifier(sortedClassNames,classifiers);
+	}
 }
