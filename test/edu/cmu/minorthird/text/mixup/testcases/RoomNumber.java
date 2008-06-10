d57aed23385a57bb2edab8b388d09fd5ea1b0704
Frank Lin <frankl@.cmu.edu>
2008-06-10 00:01:54 +0000

    removed .class file from the source tree! awful.

--- test/edu/cmu/minorthird/text/mixup/testcases/RoomNumber.java
+++ test/edu/cmu/minorthird/text/mixup/testcases/RoomNumber.java
@@ -8,23 +8,23 @@ import edu.cmu.minorthird.text.Span;
 import edu.cmu.minorthird.text.TextLabels;
 
 /** Trivial annotator used as test case. */
-public class RoomNumber extends AbstractAnnotator
-{
-	public String explainAnnotation(TextLabels labels,Span documentSpan) 
-	{ 
-		return "Not implemented"; 
+public class RoomNumber extends AbstractAnnotator{
+
+	public String explainAnnotation(TextLabels labels,Span documentSpan){
+		return "Not implemented";
 	}
-	protected void doAnnotate(MonotonicTextLabels labels)
-	{
-		for (Iterator<Span> i=labels.getTextBase().documentSpanIterator(); i.hasNext(); ) {
-			Span s = i.next();
-			for (int j=0; j<s.size(); j++) {
-				if (s.getToken(j).getValue().equals("1112")) {
-					Span r = s.subSpan(j,1);
-					labels.addToType(r, "extracted_room");
+
+	protected void doAnnotate(MonotonicTextLabels labels){
+		for(Iterator<Span> i=labels.getTextBase().documentSpanIterator();i
+				.hasNext();){
+			Span s=i.next();
+			for(int j=0;j<s.size();j++){
+				if(s.getToken(j).getValue().equals("1112")){
+					Span r=s.subSpan(j,1);
+					labels.addToType(r,"extracted_room");
 				}
 			}
 		}
-		labels.setAnnotatedBy("RoomNumber");
+		labels.setAnnotatedBy("RoomNumber2");
 	}
 }
